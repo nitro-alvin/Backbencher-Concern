224c2cfbdbe9c502255c220f0479ac71ecea989f
nitro-alvin <alvinantonyajish@gmail.com>
2024-12-28 13:40:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+//Code by Alvin
+function sub() {
+  alert("You are going to Youtube! Don't forget to Subscribe!")
+}
+function fol1() {
+  alert("You are going to Instagram! Don't forget to Follow!")
+}
+function fol2() {
+  alert("You are going to Instagram! Don't forget to Follow!")
+}
